70e5915039f63080db71a612ff64360320098b0f
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-18 23:41:43 +0530

    cricpulse

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ import createApp from "./src/app.js";
 import env from "./src/config/env.js";
 import logger from "./src/config/logger.js";
 import connectDB from "./src/config/db.js";
-
+import appConstant from "./src/constant/app.constant.js";
 const app = createApp();
 
 function startServer() {
   connectDB()      // pehle db connect then app listen
     .then(() => {
-      app.listen(env.PORT, () => {
+      app.listen(env.PORT || appConstant.PORT, () => {
         logger.info({ PORT: env.PORT }, "Server is running port");
       });
     })
