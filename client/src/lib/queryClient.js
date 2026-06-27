e5c9c4c3ff006ad81961b0cb88d9245a4b130a2f
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-27 23:48:03 +0530

    cricpulse

--- client/src/lib/queryClient.js
+++ client/src/lib/queryClient.js
@@ -1,12 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 60 * 1000,
-            refetchOnWindowFocus: false,
-        },
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+      refetchOnWindowFocus: false,
     },
+  },
 });
 
 export default queryClient;
