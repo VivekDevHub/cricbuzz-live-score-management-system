2d60012e78d4579db3b4f9e206f811a12766b53b
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-27 23:48:09 +0530

    cricpulse

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,20 +1,22 @@
 import { io } from "socket.io-client";
 
-const apiBaseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000/api";
-const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL || apiBaseUrl.replace(/\/api\/?$/, "");
+const apiBaseUrl =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000/api";
+const socketUrl =
+  process.env.NEXT_PUBLIC_SOCKET_URL || apiBaseUrl.replace(/\/api\/?$/, "");
 
 let socket;
 
 export const getSocket = () => {
-    if (!socket) {
-        socket = io(socketUrl, {
-            autoConnect: false,
-            withCredentials: true,
-            transports: ["websocket", "polling"],
-        });
-    }
+  if (!socket) {
+    socket = io(socketUrl, {
+      autoConnect: false,
+      withCredentials: true,
+      transports: ["websocket", "polling"],
+    });
+  }
 
-    return socket;
+  return socket;
 };
 
 export default getSocket;
