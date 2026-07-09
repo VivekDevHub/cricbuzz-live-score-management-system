72918a3f306bb0760685a27e41c9e80e87dc35eb
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-07-09 23:50:45 +0530

    CRICPULSE

--- client/src/app/(auth)/login/page.jsx
+++ client/src/app/(auth)/login/page.jsx
@@ -1,10 +1,10 @@
 import Login from "@/features/auth/ui/jsx/Login";
 
 export const metadata = {
-    title: "Login",
-    description: "Login to your account"
+  title: "Login",
+  description: "Login to your account",
 };
 
 export default function LoginPage() {
-    return <Login />;
+  return <Login />;
 }
